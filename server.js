2928e179d6881fc291a81707122f686581b36fec
Gul M. Javed <75537631+gulmanjaved@users.noreply.github.com>
2026-08-08 22:38:12 +0500

    Change server listen address to '0.0.0.0'

--- server.js
+++ server.js
@@ -55,4 +55,4 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Whiteboard running on http://localhost:${PORT}`));
+server.listen(PORT, "0.0.0.0", () => console.log(`Whiteboard running on port ${PORT}`));
